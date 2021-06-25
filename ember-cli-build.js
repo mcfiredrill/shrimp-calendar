b1cdc16be49c8e7224ff4f20fed766e141a39174
Tony Miller <mcfiredrill@gmail.com>
2021-06-25 12:08:28 +0900

    setup purgecss

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,19 +1,36 @@
 'use strict';
 
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const isProduction = EmberAddon.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './addon/components/**/*.hbs',
+      './app/index.html',
+      './app/templates/**/*.hbs',
+      './app/components/**/*.hbs',
+    ],
+    defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  },
+};
 
 module.exports = function (defaults) {
   let app = new EmberAddon(defaults, {
     // Add options here
-    minifyCSS: {
-      enabled: false,
-    },
-
     postcssOptions: {
       compile: {
         plugins: [
-          { module: require('postcss-import') },
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules'],
+            },
+          },
           require('tailwindcss')('./app/tailwind/config.js'),
+          ...(isProduction ? [purgeCSS] : []),
         ],
       },
     },
